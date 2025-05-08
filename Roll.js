9d26d693aa2350846b45dfb511202501bdd93a3e
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-05-08 08:38:02 -0300

    Update Roll.js

--- Roll.js
+++ Roll.js
@@ -2,19 +2,19 @@ let rollJson = [
   {
     nome: 'HOT ROLL',
     desc: 'Salmão',
-    price: 'R$: 36,00',
+    price: 'R$: 40,00',
     img: 'RollS.jpeg'
    },
   {
     nome: 'HOT ROLL',
     desc: 'Camarão',
-    price: 'R$: 45,00',
+    price: 'R$: 48,00',
     img: 'RollC.jpeg'
     },
   {
     nome: 'HOT ROLL',
     desc: 'Salmão e Kani',
-    price: 'R$: 38,00',
+    price: 'R$: 45,00',
     img: 'RollK.jpeg'
      },
   ]
