facd1a2a8d7207cfd932e895d4e583972af81026
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-05-08 08:30:24 -0300

    Update yaki.js

--- yaki.js
+++ yaki.js
@@ -2,37 +2,37 @@ let yakiJson = [
   {
     nome: 'YAKISOBA TRADICIONAL',
     desc: 'Carne e Frango',
-    price: 'R$: 55,00',
+    price: 'R$: 60,00',
     img: 'yakiT.jpeg'
    },
   {
     nome: 'YAKISOBA',
     desc: 'Frango',
-    price: 'R$: 45,00',
+    price: 'R$: 50,00',
     img: 'yakiF.jpeg'
     },
   {
     nome: 'YAKISOBA',
     desc: 'Carne',
-    price: 'R$: 50,00',
+    price: 'R$: 55,00',
     img: 'yakiC.jpeg'
      },
   {
     nome: 'YAKISOBA',
     desc: 'Legumes',
-    price: 'R$: 40,00',
+    price: 'R$: 45,00',
     img: 'yakiL.jpeg'
       },
   {
     nome: 'YAKISOBA',
     desc: 'Camarão',
-    price: 'R$: 75,00',
+    price: 'R$: 80,00',
     img: 'yakiCama.jpeg'
        },
   {
     nome: 'YAKISOBA',
     desc: 'Misto',
-    price: 'R$: 75,00',
+    price: 'R$: 80,00',
     img: 'yakiM.jpeg'
         },
   ]
