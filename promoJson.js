61173e2b4400aa1eb7ac2cf5a0fcc8b69ce30d43
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-05-08 08:19:23 -0300

    Update promoJson.js

--- promoJson.js
+++ promoJson.js
@@ -2,25 +2,25 @@ let itemPromo = [
   {
     name: 'SUSHI PIZZA',
     desc: 'Salmão e Camarão',
-    price: 'R$: 49,00',
+    price: 'R$: 55,00',
     img: 'pizza.jpeg'
   },
   {
     name: 'SUSHI DOG',
     desc: 'Grelhado',
-    price: 'R$: 45,00',
+    price: 'R$: 60,00',
     img: 'doggrel.jpeg'
   },
   {
     name: 'SUSHI DOG',
     desc: 'Cru',
-    price: 'R$: 50,00',
+    price: 'R$: 55,00',
     img: 'dogcru.jpeg'
   },
   {
     name: 'BURGUER SUSHI',
     desc: 'Salmão',
-    price: 'R$: 48,00',
+    price: 'R$: 55,00',
     img: 'hsushiburguer.jpeg'
   },
   {
